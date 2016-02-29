7ea6e733f5104a592cfb5ab5896db411dc88fc73
Elvis Pfutzenreuter <epx@neosaldina.local>
2016-02-29 18:47:12 -0300

    Move map list to model

--- SimplestGPS/GPSModel2.swift
+++ SimplestGPS/GPSModel2.swift
@@ -29,6 +29,8 @@ import CoreLocation
     var metric: Int = 1;
     var editing: Int = -1
     
+    var maps: [(img: UIImage, file: NSURL, lat0: Double, lat1: Double, long0: Double, long1: Double, latheight: Double, longwidth: Double)] = [];
+    
     override init()
     {
         super.init()
@@ -62,6 +64,115 @@ import CoreLocation
         lman!.desiredAccuracy = kCLLocationAccuracyBest
         lman!.requestAlwaysAuthorization()
         lman!.startUpdatingLocation()
+        
+        maps = []
+        
+        let fileManager = NSFileManager.defaultManager()
+        let documentsUrl = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)[0] as NSURL
+        if let directoryUrls = try? NSFileManager.defaultManager().contentsOfDirectoryAtURL(documentsUrl,
+                                                                                            includingPropertiesForKeys: nil,
+                                                                                            options:NSDirectoryEnumerationOptions.SkipsSubdirectoryDescendants) {
+            NSLog("%@", directoryUrls)
+            for url in directoryUrls {
+                let f = url.lastPathComponent!
+                let coords = parseName(f)
+                if !coords.ok {
+                    continue
+                }
+                NSLog("   map coords %f %f %f %f", coords.lat, coords.long, coords.latheight, coords.longwidth)
+                if let img = UIImage(data: NSData(contentsOfURL: url)!) {
+                    NSLog("     Image loaded")
+                    maps.append((img: img, file: url, lat0: coords.lat, lat1: coords.lat - coords.latheight,
+                        long0: coords.long, long1: coords.long + coords.longwidth,
+                        latheight: coords.latheight, longwidth: coords.longwidth))
+                } else {
+                    NSLog("     Image NOT loaded")
+                }
+            }
+        }
+    }
+    
+    func parseName(f: String) -> (ok: Bool, lat: Double, long: Double, latheight: Double, longwidth: Double)
+    {
+        NSLog("Parsing %@", f)
+        var lat = 1.0
+        var long = 1.0
+        var latheight = 0.0
+        var longwidth = 0.0
+        
+        let e = f.lowercaseString
+        let g = (e.characters.split(".").map{ String($0) }).first!
+        var h = (g.characters.split("+").map{ String($0) })
+        
+        if h.count != 4 {
+            NSLog("    did not find 4 tokens")
+            return (false, 0, 0, 0, 0)
+        }
+        if h[0].characters.count < 4 || h[0].characters.count > 6 {
+            NSLog("    latitude with <3 or >5 chars")
+            return (false, 0, 0, 0, 0)
+        }
+        
+        if h[1].characters.count < 4 || h[1].characters.count > 6 {
+            NSLog("    latitude with <3 or >5 chars")
+            return (false, 0, 0, 0, 0)
+        }
+        if h[2].characters.count < 2 || h[2].characters.count > 4 {
+            NSLog("    latheight with <3 or >4 chars")
+            return (false, 0, 0, 0, 0)
+        }
+        if h[3].characters.count < 2 || h[3].characters.count > 4 {
+            NSLog("    longwidth with <3 or >4 chars")
+            return (false, 0, 0, 0, 0)
+        }
+        
+        let ns = h[0].characters.last
+        
+        if (ns != "n" && ns != "s") {
+            NSLog("    latitude with no N or S suffix")
+            return (false, 0, 0, 0, 0)
+        }
+        if (ns == "s") {
+            lat = -1;
+        }
+        
+        let ew = h[1].characters.last
+        
+        if (ew != "e" && ew != "w") {
+            NSLog("    longitude with no W or E suffix")
+            return (false, 0, 0, 0, 0)
+        }
+        if (ew == "w") {
+            long = -1;
+        }
+        h[0] = h[0].substringToIndex(h[0].endIndex.predecessor())
+        h[1] = h[1].substringToIndex(h[1].endIndex.predecessor())
+        let ilat = Int(h[0])
+        if (ilat == nil) {
+            NSLog("    lat not parsable")
+            return (false, 0, 0, 0, 0)
+        }
+        let ilong = Int(h[1])
+        if (ilong == nil) {
+            NSLog("    long not parsable")
+            return (false, 0, 0, 0, 0)
+        }
+        let ilatheight = Int(h[2])
+        if (ilatheight == nil) {
+            NSLog("    latheight not parsable")
+            return (false, 0, 0, 0, 0)
+        }
+        let ilongwidth = Int(h[3])
+        if (ilongwidth == nil) {
+            NSLog("    longwidth not parsable")
+            return (false, 0, 0, 0, 0)
+        }
+        lat *= Double(ilat! / 100) + (Double(ilat! % 100) / 60.0)
+        long *= Double(ilong! / 100) + (Double(ilong! % 100) / 60.0)
+        latheight = Double(ilatheight! / 100) + (Double(ilatheight! % 100) / 60.0)
+        longwidth = Double(ilongwidth! / 100) + (Double(ilongwidth! % 100) / 60.0)
+        
+        return (true, lat, long, latheight, longwidth)
     }
     
     static let singleton = GPSModel2();
@@ -69,6 +180,10 @@ import CoreLocation
     class func model() -> GPSModel2
     {
         return singleton
+    }
+    
+    func get_maps() -> [(img: UIImage, file: NSURL, lat0: Double, lat1: Double, long0: Double, long1: Double, latheight: Double, longwidth: Double)] {
+        return maps
     }
     
     func sk(keys: Array<NSObject>) -> [String]
--- SimplestGPS/MapViewController.swift
+++ SimplestGPS/MapViewController.swift
@@ -23,7 +23,6 @@ import UIKit
     @IBOutlet weak var canvas: MapCanvasView!
     @IBOutlet weak var scale: UILabel!
     
-    var maps: [(img: UIImage, lat0: Double, lat1: Double, long0: Double, long1: Double, latheight: Double, longwidth: Double)] = [];
     var scrw: Double = 0
     var scrh: Double = 0
     var width_prop: Double = 1
@@ -83,118 +82,9 @@ import UIKit
         repaint()
     }
     
-    func parseName(f: String) -> (ok: Bool, lat: Double, long: Double, latheight: Double, longwidth: Double)
-    {
-        NSLog("Parsing %@", f)
-        var lat = 1.0
-        var long = 1.0
-        var latheight = 0.0
-        var longwidth = 0.0
-        
-        let e = f.lowercaseString
-        let g = (e.characters.split(".").map{ String($0) }).first!
-        var h = (g.characters.split("+").map{ String($0) })
-
-        if h.count != 4 {
-            NSLog("    did not find 4 tokens")
-            return (false, 0, 0, 0, 0)
-        }
-        if h[0].characters.count < 4 || h[0].characters.count > 6 {
-            NSLog("    latitude with <3 or >5 chars")
-            return (false, 0, 0, 0, 0)
-        }
-        
-        if h[1].characters.count < 4 || h[1].characters.count > 6 {
-            NSLog("    latitude with <3 or >5 chars")
-            return (false, 0, 0, 0, 0)
-        }
-        if h[2].characters.count < 2 || h[2].characters.count > 4 {
-            NSLog("    latheight with <3 or >4 chars")
-            return (false, 0, 0, 0, 0)
-        }
-        if h[3].characters.count < 2 || h[3].characters.count > 4 {
-            NSLog("    longwidth with <3 or >4 chars")
-            return (false, 0, 0, 0, 0)
-        }
-
-        let ns = h[0].characters.last
-        
-        if (ns != "n" && ns != "s") {
-            NSLog("    latitude with no N or S suffix")
-            return (false, 0, 0, 0, 0)
-        }
-        if (ns == "s") {
-            lat = -1;
-        }
-        
-        let ew = h[1].characters.last
-        
-        if (ew != "e" && ew != "w") {
-            NSLog("    longitude with no W or E suffix")
-            return (false, 0, 0, 0, 0)
-        }
-        if (ew == "w") {
-            long = -1;
-        }
-        h[0] = h[0].substringToIndex(h[0].endIndex.predecessor())
-        h[1] = h[1].substringToIndex(h[1].endIndex.predecessor())
-        let ilat = Int(h[0])
-        if (ilat == nil) {
-            NSLog("    lat not parsable")
-            return (false, 0, 0, 0, 0)
-        }
-        let ilong = Int(h[1])
-        if (ilong == nil) {
-            NSLog("    long not parsable")
-            return (false, 0, 0, 0, 0)
-        }
-        let ilatheight = Int(h[2])
-        if (ilatheight == nil) {
-            NSLog("    latheight not parsable")
-            return (false, 0, 0, 0, 0)
-        }
-        let ilongwidth = Int(h[3])
-        if (ilongwidth == nil) {
-            NSLog("    longwidth not parsable")
-            return (false, 0, 0, 0, 0)
-        }
-        lat *= Double(ilat! / 100) + (Double(ilat! % 100) / 60.0)
-        long *= Double(ilong! / 100) + (Double(ilong! % 100) / 60.0)
-        latheight = Double(ilatheight! / 100) + (Double(ilatheight! % 100) / 60.0)
-        longwidth = Double(ilongwidth! / 100) + (Double(ilongwidth! % 100) / 60.0)
-
-        return (true, lat, long, latheight, longwidth)
-    }
-    
     override func viewDidLoad()
     {
-        maps = []
-        
-        let fileManager = NSFileManager.defaultManager()
-        let documentsUrl = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)[0] as NSURL
-        if let directoryUrls = try? NSFileManager.defaultManager().contentsOfDirectoryAtURL(documentsUrl,
-                                                                                            includingPropertiesForKeys: nil,
-                                                                                            options:NSDirectoryEnumerationOptions.SkipsSubdirectoryDescendants) {
-            NSLog("%@", directoryUrls)
-            for url in directoryUrls {
-                let f = url.lastPathComponent!
-                let coords = parseName(f)
-                if !coords.ok {
-                    continue
-                }
-                NSLog("   map coords %f %f %f %f", coords.lat, coords.long, coords.latheight, coords.longwidth)
-                if let img = UIImage(data: NSData(contentsOfURL: url)!) {
-                    NSLog("     Image loaded")
-                    maps.append((img: img, lat0: coords.lat, lat1: coords.lat - coords.latheight,
-                        long0: coords.long, long1: coords.long + coords.longwidth,
-                        latheight: coords.latheight, longwidth: coords.longwidth))
-                } else {
-                    NSLog("     Image NOT loaded")
-                }
-            }
-        }
-        
-        let pan = UIPanGestureRecognizer(target:self, action:"pan:")
+        let pan = UIPanGestureRecognizer(target:self, action:#selector(MapViewController.pan(_:)))
         pan.maximumNumberOfTouches = 1
         pan.minimumNumberOfTouches = 1
         canvas.addGestureRecognizer(pan)
@@ -378,7 +268,7 @@ import UIKit
         canvas.send_targets(targets)
         
         var plot: [(UIImage, CGFloat, CGFloat, CGFloat, CGFloat, CGFloat)] = []
-        for map in maps {
+        for map in GPSModel2.model().get_maps() {
             if iins(map.lat0, x1: map.lat1, y0: map.long0, y1: map.long1, a: slat0, b: slat1, c: slong0, d: slong1) {
                 let x0 = long_to(map.long0, a: slong0, b: slong1)
                 let x1 = long_to(map.long1, a: slong0, b: slong1)
