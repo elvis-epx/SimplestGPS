853a9f149c7a6a7481078c333cac556436617de4
Elvis Pfutzenreuter <epx@neosaldina.local>
2016-03-18 00:52:59 -0300

    Refactoring

--- SimplestGPS/CompassBGView.swift
+++ SimplestGPS/CompassBGView.swift
@@ -21,27 +21,20 @@ class CompassBGView: UIView {
     
     func outer_circle(ctx: CGContextRef)
     {
-        var startAngle: Float = Float(2 * M_PI)
-        var endAngle: Float = 0.0
-        let radius = CGFloat((CGFloat(self.bounds.size.width)) / 2)
-        
-        let center = CGPointMake(self.bounds.size.width / 2, self.bounds.size.height / 2)
+        let radius = self.bounds.size.width / 2
+        let center = CGPointMake(self.bounds.size.width / 2,
+                                 self.bounds.size.height / 2)
         
         CGContextSetStrokeColorWithColor(ctx, UIColor.redColor().CGColor)
-        CGContextSetLineWidth(ctx, CGFloat(0))
+        CGContextSetLineWidth(ctx, 0)
         let fc = UIColor.init(colorLiteralRed: 0, green: 0, blue: 0, alpha: 0.5)
         CGContextSetFillColorWithColor(ctx, fc.CGColor)
         
-        // Rotate the angles so that the inputted angles are intuitive like the clock face: the top is 0 (or 2π), the right is π/2, the bottom is π and the left is 3π/2.
-        // In essence, this appears like a unit circle rotated π/2 anti clockwise.
-        startAngle = startAngle - Float(M_PI_2)
-        endAngle = endAngle - Float(M_PI_2)
-        CGContextAddArc(ctx, center.x, center.y, CGFloat(radius), CGFloat(startAngle), CGFloat(endAngle), 0)
+        CGContextAddArc(ctx, center.x, center.y, radius, CGFloat(2 * M_PI), 0, 1)
         CGContextDrawPath(ctx, .Fill)
     }
     
     override func drawRect(rect: CGRect) {
-        NSLog("CompassBGView drawRect")
         let context = UIGraphicsGetCurrentContext()
         if context == nil {
             return
